f546e4429c42b9c37c66917f12c0dbe404760834
Ben Steinig <bsteinig@umich.edu>
2021-03-30 15:42:36 -0400

    Spell Checking

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ function Home(){
     return(
         <div className="home">
             <h1  className="title">Timeline Generator</h1>
-            <h3>This tool uses Knightlab's Timeline.JS to create dyanmic timelines.</h3>
+            <h3>This tool uses Knightlab's Timeline.JS to create dynamic timelines.</h3>
         </div>
     )
 }
